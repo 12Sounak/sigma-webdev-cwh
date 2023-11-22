45aea7bef430c42a44efaae01e032b1591019cb0
Sounak Banerjee <aec.cse.sounakbanerjee@gmail.com>
2023-11-23 01:52:42 +0530

    javaScript4

---
+++ javaScript/video 57/index.js
@@ -0,0 +1,59 @@
+console.log("I'm a tutorial on loops");
+
+let a=1;
+// console.log(a);
+// console.log(a+1);
+// console.log(a+2);
+
+// Types of loops in jS
+/*
+1>for loop
+2>for in loop
+3>for of loop
+4>while loop
+5>do-while loop
+*/
+
+
+// for loop
+// for (let i  = 0; i  <100 ; i ++) {
+//     console.log(a+i);
+    
+// }
+
+// let obj = {
+//     name: "Sounak",
+//     role: "Programming",
+//     dept: "Data Scientist"
+// }
+
+// this prints both keys and elements 
+// for (const key in obj) {
+//     const element = obj[key];
+//         console.log(key, element);
+// }
+
+// for (const key in obj) {
+//         console.log(key);
+// }
+
+// for (const i of "Sounak") {
+//     console.log(i);
+// }
+
+
+// while loop
+// let i = 0;
+// while (i<6) {
+//     console.log(i);
+//     i++;                //removing the iteration makes it a infinite loop.
+// }
+
+
+
+// do-while loop
+let i=0;
+do {
+    console.log(i);
+    i++;
+} while (i<6);
